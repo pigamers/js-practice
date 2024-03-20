3a10703f31f8ff980041b6c0354e55a1a90690ee
Mayank Kumar Sharma <pigamers1123@gmail.com>
2024-03-20 10:42:29 +0000

    datatypes done

---
+++ 01Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";   //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs,not browser
+
+let name = "mayank"
+let age = 21;
+let LoggedIn = true;
+
+/* This is primitive datatypes
+
+// number => 2 to the power of 52 
+// bigint 
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned 
+// symbol
+
+*/
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
